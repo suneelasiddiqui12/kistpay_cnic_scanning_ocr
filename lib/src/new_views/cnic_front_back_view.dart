567b859e0e1182d9f1abe4aa51bc7a5b9e00a375
Suneela Siddiqui <suneela.siddiqui@kistpay.com>
2024-09-12 11:26:18 +0500

    KP-1480 Implement CNIC Front Scanning Screen

--- lib/src/new_views/cnic_front_back_view.dart
+++ lib/src/new_views/cnic_front_back_view.dart
@@ -16,22 +16,22 @@ class _CnicFrontBackViewState extends State<CnicFrontBackView> {
     return Scaffold(
       body: SingleChildScrollView(
         child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Image.asset('assets/images/kistpay_logo.png'),
-              const SizedBox(height: 80),
+              SizedBox(height: MediaQuery.sizeOf(context).height / 2.5),
               Center(
                 child: ElevatedButton.icon(
                   onPressed: () {
                     Navigator.push(
                       context,
-                      MaterialPageRoute(builder: (context) => CnicFrontView()),
+                      MaterialPageRoute(builder: (context) => const CnicFrontView()),
                     );
                   },
-                  icon: Icon(Icons.arrow_forward_sharp),
-                  label: Text('Start Verification', style: const TextStyle(fontSize: 12, fontWeight: FontWeight.w400)),
+                  icon: const Icon(Icons.arrow_forward_sharp),
+                  label: const Text('Start Verification', style: TextStyle(fontSize: 12, fontWeight: FontWeight.w400)),
                   style: ElevatedButton.styleFrom(
                     foregroundColor: Colors.black,
                     backgroundColor: AppColors.chineseSilver.withOpacity(0.9),
@@ -42,29 +42,9 @@ class _CnicFrontBackViewState extends State<CnicFrontBackView> {
                   ),
                 ),
               ),
-
             ],
           ),
         ),
-      ),
-    );
-  }
-  ElevatedButton _buildImagePickerButton({
-    required String label,
-    required IconData icon,
-    required VoidCallback onPressed,
-  }) {
-    return ElevatedButton.icon(
-      onPressed: onPressed,
-      icon: Icon(icon),
-      label: Text(label, style: const TextStyle(fontSize: 12, fontWeight: FontWeight.w400)),
-      style: ElevatedButton.styleFrom(
-        foregroundColor: Colors.black,
-        backgroundColor: AppColors.chineseSilver.withOpacity(0.9),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(5.0),
-        ),
-        padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 24.0),
       ),
     );
   }
