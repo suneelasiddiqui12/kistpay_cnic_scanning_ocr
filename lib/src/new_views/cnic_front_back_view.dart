e24d51e05a57a120c119fbaeb8d1dcd4db54bb33
Suneela Siddiqui <suneela.siddiqui@kistpay.com>
2024-09-12 11:18:11 +0500

    KP-1478 Implement CNIC Verification Start Screen

--- lib/src/new_views/cnic_front_back_view.dart
+++ lib/src/new_views/cnic_front_back_view.dart
@@ -22,95 +22,22 @@ class _CnicFrontBackViewState extends State<CnicFrontBackView> {
             children: [
               Image.asset('assets/images/kistpay_logo.png'),
               const SizedBox(height: 80),
-                const Text('CNIC Front', style: TextStyle(fontWeight: FontWeight.w700, fontSize: 12),),
-              const SizedBox(height: 8),
-              Container(
-                padding: const EdgeInsets.only(top: 48, bottom: 48),
-                decoration: BoxDecoration(
-                  color: AppColors.lightSilver.withOpacity(0.6),
-                  border: Border.all(color: Colors.black12),
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                child: Center(
-                  child: Column(
-                    children: [
-                      _buildImagePickerButton(
-                        label: 'Upload from Gallery',
-                        icon: Icons.camera_alt,
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => CnicFrontView()),
-                          );
-                        },
-                      ),
-                      const SizedBox(height: 8),
-                      Text('OR'),
-                      const SizedBox(height: 8),
-                      _buildImagePickerButton(
-                        label: 'Take Picture',
-                        icon: Icons.browse_gallery,
-                        onPressed: () {},
-                      ),
-                    ],
+              ElevatedButton.icon(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => CnicFrontView()),
+                  );
+                },
+                icon: Icon(Icons.arrow_forward_sharp),
+                label: Text('Start Verification', style: const TextStyle(fontSize: 12, fontWeight: FontWeight.w400)),
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.black,
+                  backgroundColor: AppColors.chineseSilver.withOpacity(0.9),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5.0),
                   ),
-                )
-              ),
-              const SizedBox(height: 29),
-              const Text('CNIC Back', style: TextStyle(fontWeight: FontWeight.w700, fontSize: 12),),
-              const SizedBox(height: 8),
-              Container(
-                  padding: const EdgeInsets.only(top: 48, bottom: 48),
-                  decoration: BoxDecoration(
-                    color: AppColors.lightSilver.withOpacity(0.6),
-                    border: Border.all(color: Colors.black12),
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: Center(
-                    child: Column(
-                      children: [
-                        _buildImagePickerButton(
-                          label: 'Upload from Gallery',
-                          icon: Icons.camera_alt,
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) => CnicBackView()),
-                            );
-                          },
-                        ),
-                        const SizedBox(height: 8),
-                        Text('OR'),
-                        const SizedBox(height: 8),
-                        _buildImagePickerButton(
-                          label: 'Take Picture',
-                          icon: Icons.browse_gallery,
-                          onPressed: () {},
-                        ),
-                      ],
-                    ),
-                  )
-              ),
-              const SizedBox(height: 38),
-              Align(
-                alignment: Alignment.centerRight,
-                child: ElevatedButton.icon(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => CnicFrontView()),
-                    );
-                  },
-                  icon: Icon(Icons.arrow_forward_sharp),
-                  label: Text('Next', style: const TextStyle(fontSize: 12, fontWeight: FontWeight.w400)),
-                  style: ElevatedButton.styleFrom(
-                    foregroundColor: Colors.black,
-                    backgroundColor: AppColors.chineseSilver.withOpacity(0.9),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(5.0),
-                    ),
-                    padding: const EdgeInsets.fromLTRB(51, 14, 51, 14),
-                  ),
+                  padding: const EdgeInsets.fromLTRB(51, 14, 51, 14),
                 ),
               ),
 
